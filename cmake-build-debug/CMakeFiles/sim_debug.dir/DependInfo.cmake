06157ba4ac94bd968c55480bf227faf0e53bed46
frehml <frederic.hamelink@hotmail.com>
2021-03-16 00:18:02 +0100

    import detection

---
+++ cmake-build-debug/CMakeFiles/sim_debug.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Frederic/Documents/PSE/Simulation/Centrum.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/Simulation/Centrum.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/Simulation/Hub.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/Simulation/Hub.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/Simulation/autoSim.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/Simulation/autoSim.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/Simulation/simulation.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/Simulation/simulation.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/Simulation/simulationExporter.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/Simulation/simulationExporter.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/Simulation/simulationImporter.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/Simulation/simulationImporter.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/XmlParser/tinystr.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/XmlParser/tinystr.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/XmlParser/tinyxml.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/XmlParser/tinyxml.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/XmlParser/tinyxmlerror.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/XmlParser/tinyxmlerror.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/XmlParser/tinyxmlparser.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/XmlParser/tinyxmlparser.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/tests/autoSimTests.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/tests/autoSimTests.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/tests/domainTests.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/tests/domainTests.cpp.obj"
+  "C:/Users/Frederic/Documents/PSE/tests_main.cpp" "C:/Users/Frederic/Documents/PSE/cmake-build-debug/CMakeFiles/sim_debug.dir/tests_main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../gtest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sim_debug.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim_debug.dir/Simulation/Centrum.cpp.obj"
+  "CMakeFiles/sim_debug.dir/Simulation/Hub.cpp.obj"
+  "CMakeFiles/sim_debug.dir/Simulation/autoSim.cpp.obj"
+  "CMakeFiles/sim_debug.dir/Simulation/simulation.cpp.obj"
+  "CMakeFiles/sim_debug.dir/Simulation/simulationExporter.cpp.obj"
+  "CMakeFiles/sim_debug.dir/Simulation/simulationImporter.cpp.obj"
+  "CMakeFiles/sim_debug.dir/XmlParser/tinystr.cpp.obj"
+  "CMakeFiles/sim_debug.dir/XmlParser/tinyxml.cpp.obj"
+  "CMakeFiles/sim_debug.dir/XmlParser/tinyxmlerror.cpp.obj"
+  "CMakeFiles/sim_debug.dir/XmlParser/tinyxmlparser.cpp.obj"
+  "CMakeFiles/sim_debug.dir/tests/autoSimTests.cpp.obj"
+  "CMakeFiles/sim_debug.dir/tests/domainTests.cpp.obj"
+  "CMakeFiles/sim_debug.dir/tests_main.cpp.obj"
+  "libsim_debug.dll.a"
+  "sim_debug.exe"
+  "sim_debug.exe.manifest"
+  "sim_debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
