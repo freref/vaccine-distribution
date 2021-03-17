29ea739cbafa7ed55247407167a7e2e7da00f5d8
SanderMar <sander.marinus@student.uantwerpen.be>
2021-03-17 19:07:53 +0100

    Add function descriptions to simulation

--- Simulation/simulation.cpp
+++ Simulation/simulation.cpp
@@ -1,6 +1,6 @@
 /**
- * University of Antwerp - BA1 Informatica - Project Software Engineering 2021
- * Definitions for the simulation class
+ \n University of Antwerp - BA1 Informatica - Project Software Engineering 2021
+ \n Definitions for the simulation class
  *
  * @authors Frederic Hamelink & Sander Marinus
  * @date    9/3/2021
@@ -12,6 +12,10 @@
 #include "Centrum.h"
 #include "Hub.h"
 #include "DesignByContract.h"
+
+//----------------------------------
+// Constructors, Destructor, checks
+//----------------------------------
 
 simulation::simulation() {
     _initCheck = this;
@@ -29,6 +33,7 @@ bool simulation::properlyInitialised() const {
 }
 
 void simulation::clear() {
+    REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling clear");
     if (hub)
         delete hub;
     hub = NULL;
@@ -39,24 +44,30 @@ void simulation::clear() {
         }
     }
     centra = vector<Centrum*>();
+    ENSURE(getHub() == NULL && getCentra().empty(), "clear postconditions failed");
 }
+
+//------------------
+// Getters, setters
+//------------------
 
 void simulation::addCentrum(Centrum* c) {
     REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling addCentrum");
     REQUIRE(c->properlyInitialised(), "centrum wasn't initialised when calling addCentrum");
+    long unsigned int oSize = centra.size();
     centra.push_back(c);
-    ENSURE(centra[centra.size()-1] == c, "addCentrum postconditions failed");
+    ENSURE(getCentra().size() == oSize+1, "addCentrum postconditions failed");
 }
 
 void simulation::setCentra(const vector<Centrum *> &c) {
-    REQUIRE(this->properlyInitialised(), "simulation zasn't initialised when calling setCentra");
+    REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling setCentra");
     REQUIRE(!c.empty(), "simulations must contain at least 1 centrum");
     centra = c;
+    ENSURE(getCentra().size() == c.size(), "setCentra postcondition failed");
 }
 
 Hub* simulation::getHub() const {
     REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling getHub");
-    REQUIRE(hub->properlyInitialised(), "hub wasn't initialised when calling getHub");
     return hub;
 }
 
@@ -64,6 +75,7 @@ void simulation::setHub(Hub *const h) {
     REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling setHub");
     REQUIRE(h->properlyInitialised(), "hub wasn't initialised when calling setHub");
     hub = h;
+    ENSURE(getHub() == h, "setHub postconditions failed");
 }
 
 const vector<Centrum*> &simulation::getCentra() const {
@@ -71,7 +83,12 @@ const vector<Centrum*> &simulation::getCentra() const {
     return centra;
 }
 
+//--------------------
+// Transport functies
+//--------------------
+
 int simulation::berekenLadingen(Centrum* centrum) const {
+    REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling berekenLadingen");
     REQUIRE(centrum->properlyInitialised(), "centrum was not initialised when calling berekenLadigen");
     int transport = hub->getTransport();
     int capaciteit = centrum->getCapaciteit();
@@ -91,28 +108,21 @@ int simulation::berekenLadingen(Centrum* centrum) const {
     return ladingen;
 }
 
-int simulation::berekenVaccinatie(Centrum* centrum){
-    REQUIRE(centrum->properlyInitialised(), "centrum was not initialised when calling berekenVaccinatie");
-    int result = min(centrum->getVaccins(), min(centrum->getCapaciteit(), centrum->getInwoners()-centrum->getGevaccineerd()));
-    ENSURE(result>=0 && result <= centrum->getCapaciteit(), "berekenVaccinatie postconditions failed");
-    return result;
+//verhoogt de vaccins in een hub
+void simulation::verhoogVaccinsHub(int vaccins) {
+    REQUIRE(getHub()->properlyInitialised(), "hub wasn't properly initialised when calling verhoogVaccinsHub");
+    REQUIRE(vaccins>=0, "vaccins amount must be positive");
+    hub->setVoorraad(hub->getVoorraad()+vaccins);
 }
 
 //verlaagt het aantal vaccins in de hub met i aantal vaccins
 void simulation::verlaagVaccinsHub(int vaccins){
-    REQUIRE(hub->properlyInitialised(), "hub wasn't initialised when calling verlaagVaccinsHub");
+    REQUIRE(getHub()->properlyInitialised(), "hub wasn't initialised when calling verlaagVaccinsHub");
     REQUIRE(vaccins >= 0, "vaccins amount must be positive");
     int before = hub->getVoorraad();
     hub->setVoorraad(hub->getVoorraad()-vaccins);
-    ENSURE(hub->getVoorraad() == before-vaccins && hub->getVoorraad() >= 0,
+    ENSURE(getHub()->getVoorraad() == before-vaccins && getHub()->getVoorraad() >= 0,
            "verlaagVaccinsHub postconditions failed");
-}
-
-//verhoogt de vaccins in een hub
-void simulation::verhoogVaccinsHub(int vaccins) {
-    REQUIRE(hub->properlyInitialised(), "hub wasn't properly initialised when calling verhoogVaccinsHub");
-    REQUIRE(vaccins>=0, "vaccins amount must be positive");
-    hub->setVoorraad(hub->getVoorraad()+vaccins);
 }
 
 //verhoogt de vaccins in gegeven centrum met i aantal vaccins
@@ -122,9 +132,29 @@ void simulation::verhoogVaccinsCentrum(Centrum* centrum, int vaccins){
     int oAmount = centrum->getVaccins();
     int capacity = centrum->getCapaciteit();
     centrum->setVaccins(centrum->getVaccins()+vaccins);
-    ENSURE(centrum->getVaccins() == oAmount+vaccins && centrum->getVaccins() <= capacity*2,
+    ENSURE(centrum->getVaccins() == oAmount+vaccins && centrum->getVaccins() <= centrum->getCapaciteit()*2,
            "verhoogVaccinsCentrum postconditions failed");
 }
+
+void simulation::printTransport(Centrum* centrum, int vaccins, ostream& onStream) const {
+    REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling printTransport");
+    REQUIRE(vaccins >= 0, "vaccins amount can't be negative");
+    REQUIRE(vaccins%getHub()->getTransport() == 0, "vaccins amount must be multiple of transport in hub");
+    onStream << "Er werden " << vaccins/hub->getTransport() << " ladingen (" << vaccins << " vaccins)"
+             << " getransporteerd naar "<< centrum->getNaam() <<"." << endl;
+}
+
+//---------------------
+// Vaccinatie functies
+//---------------------
+
+int simulation::berekenVaccinatie(Centrum* centrum){
+    REQUIRE(centrum->properlyInitialised(), "centrum was not initialised when calling berekenVaccinatie");
+    int result = min(centrum->getVaccins(), min(centrum->getCapaciteit(), centrum->getInwoners()-centrum->getGevaccineerd()));
+    ENSURE(result>=0 && result <= centrum->getCapaciteit(), "berekenVaccinatie postconditions failed");
+    return result;
+}
+
 
 //verlaagt het aantal vaccins in gegeven centrum
 void simulation::verlaagVaccinCentrum(Centrum* centrum, int vaccins){
@@ -143,14 +173,6 @@ void simulation::verhoogVaccinaties(Centrum* centrum, int vaccins){
     centrum->setGevaccineerd(centrum->getGevaccineerd()+vaccins);
     ENSURE(centrum->getGevaccineerd() <= centrum->getInwoners(),
            "verhoogVaccinaties postconditions failed");
-}
-
-void simulation::printTransport(Centrum* centrum, int vaccins, ostream& onStream) const {
-    REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling printTransport");
-    REQUIRE(vaccins >= 0, "vaccins amount can't be negative");
-    REQUIRE(vaccins%hub->getTransport() == 0, "vaccins amount must be multiple of transport in hub");
-    onStream << "Er werden " << vaccins/hub->getTransport() << " ladingen (" << vaccins << " vaccins)"
-         << " getransporteerd naar "<< centrum->getNaam() <<"." << endl;
 }
 
 void simulation::printVaccinatie(Centrum* centrum, int vaccins, ostream& onStream) {
--- Simulation/simulation.h
+++ Simulation/simulation.h
@@ -1,6 +1,6 @@
 /**
- * University of Antwerp - BA1 Informatica - Project Software Engineering 2021
- * Class which stores and handles the simulation
+ \n University of Antwerp - BA1 Informatica - Project Software Engineering 2021
+ \n Class which stores and handles the simulation
  *
  * @authors Frederic Hamelink & Sander Marinus
  * @date    9/3/2021
@@ -14,41 +14,160 @@
 
 using namespace std;
 
+// Forward declaration of necessary classes
 class Hub;
 class Centrum;
 
-/**
- * Class which represents a vaccination simulation
- */
+
 class simulation {
     Hub* hub;
     vector<Centrum*> centra;
     simulation* _initCheck;
 public:
+    /**
+     \n Constructor
+     \n ENSURE(this->properlyInitialised(), "constructor must end properlyInitialised");
+     */
     simulation();
+    /**
+     \n Destructor
+     */
     ~simulation();
+    /**
+     \n Initialisation check
+     * @return  Whether class is initialised
+     */
     bool properlyInitialised() const;
 
+    /**
+     \n Clears the simulation class
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling clear")
+     \n ENSURE(getHub() == NULL && getCentra().empty(), "clear postconditions failed")
+     */
     void clear();
 
+    /**
+     \n Sets the hub in the simulation
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling setHub")
+     \n REQUIRE(h->properlyInitialised(), "hub wasn't initialised when calling setHub")
+     \n ENSURE(getHub() == h, "setHub postconditions failed")
+     *  @param h The hub to be added
+     */
     void setHub(Hub* const h);
+    /**
+     \n Add a centrum to the centra in the simulation
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling addCentrum")
+     \n REQUIRE(c->properlyInitialised(), "centrum wasn't initialised when calling addCentrum")
+     \n ENSURE(getCentra().size() == oSize+1, "addCentrum postconditions failed")
+     *  @param c    to be added centra
+     */
     void addCentrum(Centrum* c);
+    /**
+     \n Set all centra in simulation
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling setCentra")
+     \n REQUIRE(!c.empty(), "simulations must contain at least 1 centrum")
+     \n ENSURE(getCentra().size() == c.size(), "setCentra postcondition failed")
+     *  @param c    The vector containing to ba added centra
+     */
     void setCentra(const vector<Centrum*>& c);
 
+    /**
+     \n Get the simulations hub
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling getHub")
+     *  @return The hub
+     */
     Hub* getHub() const;
+    /**
+     \n Get the centra in the simulation
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling getCentra")
+     *  @return The centra
+     */
     const vector<Centrum*> &getCentra() const;
 
-    //transport
+    // Transport functies
+    /**
+     \n Calculate amount of needed loads for transport
+     \n REQUIRE(this->properlyInitialised(), "simulation wasn't initialised when calling berekenLadingen")
+     \n REQUIRE(centrum->properlyInitialised(), "centrum was not initialised when calling berekenLadigen")
+     *  @param centrum  Destination centrum
+     *  @return         Amount of loads
+     */
     int berekenLadingen(Centrum* centrum) const;
+    /**
+     \n Increases amount of vaccins in the hub
+     \n REQUIRE(getHub()->properlyInitialised(), "hub wasn't properly initialised when calling verhoogVaccinsHub")
+     \n REQUIRE(vaccins>=0, "vaccins amount must be positive")
+     * @param vaccins   Amount of vaccins to add
+     */
     void verhoogVaccinsHub(int vaccins);
+    /**
+     \n Decrease the amount of vaccins in the hub
+     \n REQUIRE(getHub()->properlyInitialised(), "hub wasn't initialised when calling verlaagVaccinsHub")
+     \n REQUIRE(vaccins >= 0, "vaccins amount must be positive")
+     \n ENSURE(getHub()->getVoorraad() == before-vaccins && getHub()->getVoorraad() >= 0,
+                "verlaagVaccinsHub postconditions failed")
+     * @param vaccins   Amount of vaccins to be subtracted
+     */
     void verlaagVaccinsHub(int vaccins);
+    /**
+     \n Increases the amount of vaccins in the centrum
+     \n REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling verhoogVaccinsCentrum")
+     \n REQUIRE(vaccins>=0, "vaccins amount must be positive")
+     \n ENSURE(centrum->getVaccins() == oAmount+vaccins && centrum->getVaccins() <= centrum->getCapaciteit()*2,
+                "verhoogVaccinsCentrum postconditions failed")
+     * @param centrum   Centrum where to increase vaccins
+     * @param vaccins   Amount of vaccins to add
+     */
     static void verhoogVaccinsCentrum(Centrum* centrum, int vaccins);
+    /**
+     \n Prints the transport
+     \n REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling printTransport")
+     \n REQUIRE(vaccins >= 0, "vaccins amount can't be negative")
+     \n REQUIRE(vaccins%getHub()->getTransport() == 0, "vaccins amount must be multiple of transport in hub")
+     * @param centrum   The destination centrum
+     * @param vaccins   The amount of transported vaccins
+     * @param onStream  The output stream
+     */
     void printTransport(Centrum* centrum, int vaccins, ostream& onStream) const;
 
-    //vaccinaties
+    // Vaccinatie functies
+    /**
+     \n Calculate the amount of vaccins that can be given in a centrum
+     \n REQUIRE(centrum->properlyInitialised(), "centrum was not initialised when calling berekenVaccinatie")
+     \n ENSURE(result>=0 && result <= centrum->getCapaciteit(), "berekenVaccinatie postconditions failed")
+     * @param centrum   The centrum for which to calculate the amount
+     * @return          The amount of vaccins
+     */
     static int berekenVaccinatie(Centrum* centrum);
+    /**
+     \n Decrease the amount of vaccins in a given centrum
+     \n REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling verlaagVaccinsCentrum")
+     \n REQUIRE(vaccins>=0, "vaccins amount must be positive")
+     \n ENSURE(centrum->getVaccins() == oAmount-vaccins && centrum->getVaccins() >= 0,
+                "verlaagVaccinsCentrum postconditions failed")
+     * @param centrum   Centrum in which to decrease the amount
+     * @param vaccins   Amount of vaccins to subtract
+     */
     static void verlaagVaccinCentrum(Centrum* centrum, int vaccins);
+    /**
+     \n Increase the amount of vaccinated people in a center
+     \n REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling verhoogVaccinaties")
+     \n REQUIRE(vaccins >= 0, "vaccins amount must be positive")
+     \n ENSURE(centrum->getGevaccineerd() <= centrum->getInwoners(),
+                "verhoogVaccinaties postconditions failed");
+     * @param centrum   Centrum in which to increase amount
+     * @param vaccins   Amount of vaccinated people
+     */
     static void verhoogVaccinaties(Centrum* centrum, int vaccins);
+    /**
+     \n Prints the vaccinations
+     \n REQUIRE(centrum->properlyInitialised(), "centrum wasn't initialised when calling printVaccinatie")
+     \n REQUIRE(vaccins >= 0, "vaccinated amount can't be negative")
+     \n REQUIRE(vaccins <= centrum->getCapaciteit(), "vaccinated ammount can't exceed capacity")
+     * @param centrum   Centrum in which the vaccination took place
+     * @param vaccins   Amount of vaccinated people
+     * @param onStream  Output stream
+     */
     static void printVaccinatie(Centrum* centrum, int vaccins, ostream& onStream);
 };
 
